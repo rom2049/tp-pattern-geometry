9dcc3aae85ff2f61035a2f4767a32ba507096856
formation <formation@ubuntu.ubuntu-domain>
2023-12-13 13:16:07 +0100

    mise à jour du modèle de test

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -8,11 +8,11 @@ public class CoordinateTest {
 	public static final double EPSILON = 1.0e-15;
 
 	@Test
-	public void testDefaultConstructor(){
+	public void testConstructorXY(){
 		// TODO
-		//Coordinate c = new Coordinate();
-		//Assert.assertEquals(0.0, c.getX(), EPSILON);
-		//Assert.assertEquals(0.0, c.getY(), EPSILON);
+		//Coordinate c = new Coordinate(3.0,4.0);
+		//Assert.assertEquals(3.0, c.getX(), EPSILON);
+		//Assert.assertEquals(4.0, c.getY(), EPSILON);
 	}
 
 }
